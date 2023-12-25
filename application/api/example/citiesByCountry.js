27a49324aadc18ea347ed5ef681bad17957d29d0
Vladyslav Karpenko <klarpen@gmail.com>
2023-12-25 13:34:57 +0200

    Executing a raw query with Knex

--- application/api/example/citiesByCountry.js
+++ application/api/example/citiesByCountry.js
@@ -5,10 +5,22 @@ async ({ countryId }) => {
   // const data = await db.pg.select('City', fields, where);
 
   // Knex Query Builder
-  const data = await db
+  /* const data = await db
     .knex('City')
     .select(['cityId', 'name'])
-    .where({ countryId });
+    .where({ countryId }); */
+
+  // Raw query execution
+  // https://knexjs.org/guide/raw.html
+  const data = await db.knex.raw(
+    'SELECT "cityId", "name" FROM "City" WHERE "countryId" = ?',
+    [countryId],
+  );
+  // broken official example https://knexjs.org/guide/query-builder.html#fromraw
+  // error: select * from (SELECT * FROM "City" WHERE "countryId" = $1) - subquery in FROM must have an alias
+  /* const data = await db.knex
+    .select('*')
+    .fromRaw('(SELECT * FROM "City" WHERE "countryId" = ?)', countryId); */
 
   return { result: 'success', data };
 };
